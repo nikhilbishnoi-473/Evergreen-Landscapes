307d46b6091a832269320d47edbc03d353072d0a
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-02-29 00:54:41 -1200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+}
+    
+const mytimeout = setTimeout(layout,3000)
+function  layout()
+{
+    document.getElementById("preloader").classList.remove("image")
+    document.getElementById("img").style.display ="none"
+}
+
