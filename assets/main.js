a965c02d22026d58db5e0c63d8d672e1bce55282
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-02 15:44:45 -1200

    page update

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,14 @@
+// const about_us =document.querySelectorAll(".about_us")
+// about_us.forEach(e => e.addEventListener("click"), () => {
+//     document.body.classList.remove("overflow_hidden")
+//     document.getElementById("navbar").classList.toggle("start-0")
+//     document.querySelector(".menu").classList.toggle("cross")
+// })
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
+    document.getElementById("navbar").classList.toggle("start-0")
+    document.body.classList.toggle("overflow_hidden")
     document.querySelector(".menu").classList.toggle("cross")
+
 }
     
 const mytimeout = setTimeout(layout,3000)
